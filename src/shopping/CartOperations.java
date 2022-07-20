2a4f9dbfe7db102b0b88bcd1029c1e2ac5eb4632
cassiano-dio <cassiano@dio.me>
2022-07-20 11:58:03 -0300

    Commit 01

---
+++ src/shopping/CartOperations.java
@@ -0,0 +1,15 @@
+package shopping;
+
+public abstract class CartOperations {
+
+    public abstract void getCartTotalPrice();
+
+    // public void orderCartDesc(){}
+
+    // public void orderCartAsc(){}
+
+    public void listCartPrices(){}
+
+    public void itemsCount(){}
+    
+}
